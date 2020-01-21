76ed8c46318a49672151f4c34afb05af0470ae85
Chad Looker <chad.looker@gmail.com>
2020-01-21 08:14:00 -0500

    Add images to component-library

--- component-library/src/app/mods/mods-routing.module.ts
+++ component-library/src/app/mods/mods-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { ModalComponent } from './modal/modal.component';
 import { ModsHomeComponent } from './mods-home/mods-home.component';
 
 const routes: Routes = [
